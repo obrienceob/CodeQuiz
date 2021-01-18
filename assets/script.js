cf07135aa25e148071d8dead477a3cd269ad46f8
Conor O'Brien <obrienceob@gmail.com>
2021-01-17 17:26:07 -0800

    defined quiz variables and questions

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+var quizQuestions = [
+    {
+        question: "Which of the following is NOT a common datatype?",
+        options: ["strings", "boolean", "number", "element"],
+        answer: ["element"]
+    },
+    {
+        question: "what is 'this' in JavaScript?",
+        options: ["An english word used to identify a specific person or thing close at hand or being indicated or experienced", "The this keyword refers to the object it belongs to", "A question in your quiz, dummy", "...this is fine"],
+        answer: ["The this keyword refers to the object it belongs to"]
+    },
+    {
+        question: "A useful tool available in Chrome to help catch JavaScript errors is: ",
+        options: ["Elements", "Window", "Console", "console log"],
+        answer: ["Console"]
+    },
+    {
+        question: "When making a function, where should I store the lines of code to be executed?",
+        options: ["In the parenthesis", "within quotes", "[]", "Curly braces"],
+        answer: ["Curly braces"]
+    },
+    {
+        question: "What is the DOM?",
+        options: ["Document Object Model", "A suffix forming nouns denoting a state or condition. Ex- freedom", "One of the main characters in the Fast and the Furious movies, played by Vin Diesel", "Document Object Method"],
+        answer: ["Document Object Model"]
+    }
+]
+
+var score = 0
+var questionIndex = 0 
