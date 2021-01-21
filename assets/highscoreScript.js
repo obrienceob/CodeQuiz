3bb25b2c5e989063ca6577c21c33b02e4e412b36
Conor O'Brien <obrienceob@gmail.com>
2021-01-20 17:59:08 -0800

    added psuedocode

--- assets/highscoreScript.js
+++ assets/highscoreScript.js
@@ -1,12 +1,16 @@
-var goBack = document.querySelector("#goBack");
+//defining the variable as buttons to select
+var goQuiz = document.querySelector("#goBack");
 var clear = document.querySelector("#clear");
 var highScoreList = document.querySelector("#highScoreList");
 
+
+//clearing the highscores
 clear.addEventListener("click", function() {
     localStorage.clear();
     location.reload();
 });
 
+//grabbing the highscores from local storage, then displaying them
 var highScores = localStorage.getItem("highScores");
 highScores = JSON.parse(highScores);
 
@@ -20,7 +24,7 @@ if (highScores !== null) {
 
     };
 };
-// Event listener to move to index page
-goBack.addEventListener("click", function () {
+// button to go back to the index page
+goQuiz.addEventListener("click", function () {
     window.location.replace("./index.html");
 });
